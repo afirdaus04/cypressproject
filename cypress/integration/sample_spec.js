a35322c72cc71b150746ef19401307f8ef24917c
afirdaus04 <afirdaus.pe@gmail.com>
2022-05-26 14:17:20 +0800

    sample spec complete

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,15 @@
-// sample_spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+describe('My First Test', function(){
+    it('Visits the kitchen sink', function(){
+        cy.visit('https://example.cypress.io')
+
+        cy.pause()
+
+        cy.contains('type').click()
+
+        cy.url().should('include', '/commands/actions')
+
+        cy.get('.action-email')
+          .type('fake@email.com')
+          .should('have.value', 'fake@email.com')
+    })
+})
